8b28cebe15bf3e516b2cb9b88dd6a65fcbe120aa
Daniel Pinto <apintod@uninorte.edu.co>
2022-08-19 14:26:40 -0500

    Inyectando dependencias con interfases

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-using webAPIapi.Middlewares;
+//using webAPIapi.Middlewares;
+
+using webAPIapi.Interfaces;
+using webAPIapi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddScoped<HelloWorldInterface, HelloWorldService>();
+builder.Services.AddScoped(p => new HelloWorldService());
 
 var app = builder.Build();
 
